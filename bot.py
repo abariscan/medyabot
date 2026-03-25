ce9daab922d38255938ad06cc53892a8fcc467af
abariscan <41114127+abariscan@users.noreply.github.com>
2026-03-25 22:52:40 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -34,9 +34,10 @@ def video_indir(url, dosya_adi, sadece_ses=False):
         'outtmpl': dosya_adi,
         'quiet': True,
         'no_warnings': True,
-        'cookiefile': cookie_path, # Tam dosya yolunu veriyoruz
-        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0',
-        'referer': 'https://www.instagram.com/',
+        'cookiefile': 'cookies.txt', 
+        # Aşağıdaki 3 satırı mutlaka ekle/güncelle:
+        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        'referer': 'https://www.google.com/',
         'nocheckcertificate': True,
     }
     
